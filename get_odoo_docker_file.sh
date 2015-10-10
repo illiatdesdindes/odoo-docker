93520101859fc09e5902782b7747b567bd3a731d
Samy VACHERESSE <samy@samyv.com>
2015-10-10 23:54:41 +0200

    add: postgres

--- get_odoo_docker_file.sh
+++ get_odoo_docker_file.sh
@@ -3,3 +3,6 @@
 curl https://raw.githubusercontent.com/odoo/docker/master/9.0/Dockerfile > Dockerfile
 curl https://raw.githubusercontent.com/odoo/docker/master/9.0/entrypoint.sh > entrypoint.sh
 curl https://raw.githubusercontent.com/odoo/docker/master/9.0/openerp-server.conf > openerp-server.conf
+
+curl https://raw.githubusercontent.com/docker-library/postgres/master/9.4/Dockerfile > postgres/Dockerfile
+curl https://raw.githubusercontent.com/docker-library/postgres/master/9.4/docker-entrypoint.sh > postgres/docker-entrypoint.sh
