4a3992f69bce08433a533b5605b11edd84c2907b
Samy VACHERESSE <samy@samyv.com>
2015-10-10 23:19:30 +0200

    init

---
+++ get_odoo_docker_file.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+curl https://raw.githubusercontent.com/odoo/docker/master/9.0/Dockerfile > Dockerfile
+curl https://raw.githubusercontent.com/odoo/docker/master/9.0/entrypoint.sh > entrypoint.sh
+curl https://raw.githubusercontent.com/odoo/docker/master/9.0/openerp-server.conf > openerp-server.conf
